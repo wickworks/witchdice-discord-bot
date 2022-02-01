063b5cbeb8edbddcaf1f1b1780cba1a051cb83c8
Olive Perry <wick@nitidbit.com>
2022-02-01 10:51:10 -0800

    convert to using a Cog

--- main.py
+++ main.py
@@ -1,244 +1,42 @@
 import os
 import discord
-import pyrebase
 import requests
 import json
 import asyncio
 
+from monitor_streams import MonitorStreams
+
 from discord.ext import commands
 from dotenv import load_dotenv
+
 
 load_dotenv()
 WITCHBOT_TOKEN = os.getenv('WITCHBOT_TOKEN')
 
 bot = commands.Bot(command_prefix='~', description='Witchdice Discord Integration')
 
-config = {
-    "apiKey": "AIzaSyBQJ2LG4nrCBhoIxg94rYi7AzzNf-GqgTM",
-    "authDomain": "roll-to-hit.firebaseapp.com",
-    "databaseURL": "https://roll-to-hit.firebaseio.com",
-    "storageBucket": "roll-to-hit.appspot.com"
-}
+if __name__ == '__main__':
+    bot.add_cog(MonitorStreams(bot))
 
-firebase = pyrebase.initialize_app(config)
-db = firebase.database()
+@bot.event
+async def on_ready():
+    print(f'\n\nLogged in as: {bot.user.name} - {bot.user.id}\nVersion: {discord.__version__}\n')
 
-rooms = {}
+bot.run(WITCHBOT_TOKEN, bot=True, reconnect=True)
 
-# Process data coming from Witchdice
-async def roll_stream_handler(message):
-    global rooms
-
-    print(f"roll_stream_handler for message {message}")
-
-    data = message["data"]
-    room_name = message["stream_id"]
-
-    if room_name in rooms.keys():
-        for channel_id in rooms[room_name]:
-            channel = bot.get_channel(int(channel_id))
-
-            embed = parse_roll(data)
-
-            try:
-                await channel.send(f"_{data.get('name', '[UNKNOWN]')}_", embed=embed)
-
-            except KeyError as e:
-                print(e)
-    else:
-        print(f"{room_name}: Not found in current list of rooms... what?")
-
-
-@bot.command(pass_context=True)
-async def add_room(ctx, room_name: str):
-    print(f"ADDING ROOM {room_name}")
-
-    global rooms
-    l_room_name = room_name.strip().lower()
-
-    if l_room_name in rooms.keys():
-        rooms[l_room_name] = rooms[l_room_name].append(ctx.message.channel.id)
-    else:
-        rooms[l_room_name] = [ctx.message.channel.id]
-        create_stream(l_room_name)
-
-    # Save the re-load room
-    open('rooms.json', 'w').write(json.dumps(rooms, indent=2))
-    rooms = json.load(open('rooms.json', 'r'))
-
-
-async def room_check():
-    global rooms
-    streams = {}
-
-    print("Loading Rooms")
-    rooms = json.load(open('rooms.json', 'r'))
-
-    print("Bot Wait Until Ready")
-    await bot.wait_until_ready()
-
-    print("Creating Streams")
-    for stream_path in set(rooms.keys()):
-        streams[stream_path] = create_stream(stream_path)
-        # create_stream(stream_path)
-
-    print("Looping stream monitor")
-    while True:
-        print("Beep")
-        for stream_name, stream_feed in streams.items():
-            for msg in stream_feed:
-                print(f"found message in stream! {msg}")
-                if msg:
-                    msg_data = json.loads(msg.data)
-                    if msg_data['data'] is not None:
-                        print(f"   msg_data! {msg_data}")
-                        msg_data["event"] = msg.event
-                        msg_data["stream_id"] = stream_name
-                        await roll_stream_handler(msg_data)
-
-        print("Boop")
-        await asyncio.sleep(1)
-
-
-def create_stream(room_name):
-    print(f"creating stream for room : {room_name}")
-    try:
-        stream = db.child("rolls").child(room_name)
-        sse = pyrebase.pyrebase.ClosableSSEClient(stream.build_request_url(None), session=pyrebase.pyrebase.KeepAuthSession(), build_headers=stream.build_headers)
-
-        print(f"     created stream:{stream}")
-        return sse
-
-    except requests.exceptions.HTTPError as e:
-        print(e)
-        return None
-
-
-
-def parse_roll(roll_msg_data):
-    print(roll_msg_data)
-
-    content_text = ""  # goes in message content
-    decorative_text = ""  # goes in embed author name
-    result_text = ""  # goes in embed title
-    rolls_text = ""  # goes in embed description
-
-    summary_mode = roll_msg_data.get("conditions")  # total | high | low
-
-    # extracts the roll objects from the firebase data
-    all_rolls = [roll[1] for roll in roll_msg_data.items() if "roll" in roll[0]]
-
-    # Collect all results by dietype e.g. {'d4':[2,3,3], 'd20':[20]}
-    results_by_dietype = {}
-    for roll in all_rolls:
-        dietype = roll["dieType"]
-        result = roll["result"]
-        if dietype in results_by_dietype:
-            results_by_dietype[dietype].append(result)
-        else:
-            results_by_dietype[dietype] = [result]
-
-    # ~ character name ~
-    content_text = roll_msg_data.get("name", "[UNKNOWN]")
-
-    # ~ extra decorative text ~
-    decorative_text = "🌺 💀 🌺"
-
-    # ~ summary // total ~
-    if summary_mode == "total":
-        all_values = [val["result"] for val in all_rolls if "result" in val.keys()]
-        result_text = str(sum(all_values)).rjust(2, " ")
-
-    if summary_mode == "high":
-        high_values = [max(results) for results in results_by_dietype.values()]
-        result_text = str(sum(high_values)).rjust(2, " ")
-
-    if summary_mode == "low":
-        low_values = [min(results) for results in results_by_dietype.values()]
-        result_text = str(sum(low_values)).rjust(2, " ")
-
-    result_text = f"-{{ {result_text} }}-"
-
-    # ~ what-was-rolled graph ~    (or one-liner if it was a simple roll)
-    if len(all_rolls) == 1:
-        rolls_text = f"```-{{ {list(results_by_dietype)[0]} }}-```"
-
-    else:
-        # first "column" is five spaces wide, "total" | "  min" | "  max"
-        type_line = " " * 5
-        mode_text = {"low": "min", "high": "max", "total": "total"}
-        results_line = (
-            mode_text[summary_mode] if summary_mode in mode_text else ""
-        ).rjust(5, " ")
-
-        # the following columns are: die type on top, results grouped below.
-        group_join_char = "+" if summary_mode == "total" else ","
-        for dietype in results_by_dietype:
-
-            # die type is easy
-            type_column = str(dietype)
-
-            # results need to be joined together with either a comma or plus, depending on the summation mode
-            result_strings = [str(result) for result in results_by_dietype[dietype]]
-            result_column = f"({group_join_char.join(result_strings)})"
-
-            # everything but the last group gets a +
-            if dietype != list(results_by_dietype)[-1]:
-                result_column += " +"
-
-            # keep the column the same length by padding the shorter row with spaces
-            column_width = max(len(type_column), len(result_column))
-            type_column = type_column.ljust(column_width, " ")
-            result_column = result_column.ljust(column_width, " ")
-
-            type_line += f" {type_column}"
-            results_line += f" {result_column}"
-
-        rolls_text = f"```{type_line}\n{results_line}```"
-
-    # print(content_text)
-    # print(decorative_text)
-    # print(result_text)
-    # print(rolls_text)
-
-    # TODO: content_text needs to get to the bot.say() somehow; either return it alongside the embed or just do the .get("name") out there.
-    embed = discord.Embed(
-        title=result_text,
-        description=rolls_text,
-        color=discord.Color(int("0x511D20", 16)),
-    )  # Mutter the incantation and perform the hex
-    embed.set_author(name=decorative_text)
-
-    # embed.set_footer(text="room-name-here")
-    # embed.set_image()
-    # embed.set_thumbnail()
-
-    # Can't have empty fiends or it gets mad. { Olive: that github visualizer seemed to think that [] was an ok value for "fields"? Can we skip it? }
-    # if field_text == "":
-    #     field_text = "[UNK]"
-    # if embed_text.strip() == "":
-    #     embed_text = "[UNK]"
-    #
-    # embed.add_field(name=field_text, value=embed_text, inline=False)
-
-    return embed
-
-
-if __name__ == "__main__":
-    test_roll = {
-        "conditions": "low",
-        "createdAt": 1638743577914,
-        "name": "TestTest",
-        "roll-0": {"dieType": "d20", "result": 14, "sign": 1},
-        "roll-1": {"dieType": "d12", "result": 10, "sign": 1},
-        "roll-2": {"dieType": "d12", "result": 6, "sign": 1},
-        "roll-3": {"dieType": "d8", "result": 8, "sign": 1},
-        "type": "dicebag",
-        "updatedAt": 1638743577914,
-    }
-
-    parse_roll(test_roll)
-
-    bot.loop.create_task(room_check())
-
-    bot.run(WITCHBOT_TOKEN)
+# if __name__ == "__main__":
+#     test_roll = {
+#         "conditions": "low",
+#         "createdAt": 1638743577914,
+#         "name": "TestTest",
+#         "roll-0": {"dieType": "d20", "result": 14, "sign": 1},
+#         "roll-1": {"dieType": "d12", "result": 10, "sign": 1},
+#         "roll-2": {"dieType": "d12", "result": 6, "sign": 1},
+#         "roll-3": {"dieType": "d8", "result": 8, "sign": 1},
+#         "type": "dicebag",
+#         "updatedAt": 1638743577914,
+#     }
+#
+#     parse_roll(test_roll)
+#
+#     bot.run(WITCHBOT_TOKEN)
---
+++ monitor_streams.py
@@ -0,0 +1,238 @@
+import os
+import pyrebase
+import json
+import discord
+
+from discord.ext import tasks, commands
+
+
+class MonitorStreams(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+        # configure firebase db connection
+        config = {
+            "apiKey": os.getenv('FIREBASE_API_KEY'),
+            "authDomain": "roll-to-hit.firebaseapp.com",
+            "databaseURL": "https://roll-to-hit.firebaseio.com",
+            "storageBucket": "roll-to-hit.appspot.com"
+        }
+        self.db = pyrebase.initialize_app(config).database()
+
+        # initialize the room and stream variables
+        self.rooms = {}
+        self.streams = {}
+
+        # Kick off the monitoring background task
+        self.monitor_room_streams.start()
+
+
+    def cog_unload(self):
+        print('   Unloading monitor_streams cog, have a nice day.')
+
+    # Check streams for changes from firebase every second
+    @tasks.loop(seconds=5.0)
+    async def monitor_room_streams(self):
+        print(f"Checking streams... {self.streams}")
+
+        for stream_name, stream_feed in self.streams.items():
+            print(f"looking in stream.... {stream_name}")
+
+            for msg in stream_feed:
+                print(f"   found message in stream! {msg}")
+                if msg:
+                    msg_data = json.loads(msg.data)
+                    if msg_data['data'] is not None:
+                        print(f"   msg_data! {msg_data}")
+                        msg_data["event"] = msg.event
+                        msg_data["stream_id"] = stream_name
+                        await self.roll_stream_handler(msg_data)
+                        print('+++++ REALLY finished waiting for roll stream event +++')
+
+    # Initialize 'rooms' from rooms.json and creates corresponding streams.
+    @monitor_room_streams.before_loop
+    async def initialize_streams(self):
+        print('Waiting for bot to be ready...')
+        await self.bot.wait_until_ready()
+
+        print("Loading Rooms")
+        self.rooms = json.load(open('rooms.json', 'r'))
+
+        print("Creating Streams:")
+        for stream_path in set(self.rooms.keys()):
+            print(stream_path)
+            self.streams[stream_path] = self.create_stream(stream_path)
+
+
+    # Create a persistant connection to firebase to listen for changes in a room
+    def create_stream(self, room_name):
+        print(f"creating stream for room : {room_name}")
+        try:
+            stream = self.db.child("rolls").child(room_name)
+            sse = pyrebase.pyrebase.ClosableSSEClient(stream.build_request_url(None), session=pyrebase.pyrebase.KeepAuthSession(), build_headers=stream.build_headers)
+
+            print(f"     created stream:{stream}")
+            return sse
+
+        except requests.exceptions.HTTPError as e:
+            print(e)
+            return None
+
+    # Process data coming from Witchdice
+    async def roll_stream_handler(self, message):
+        print(f"      roll_stream_handler for message {message}")
+
+        data = message["data"]
+        room_name = message["stream_id"]
+
+        if room_name in self.rooms.keys():
+            for channel_id in self.rooms[room_name]:
+                channel = self.bot.get_channel(int(channel_id))
+
+                embed = parse_roll(data)
+
+                try:
+                    print(f"     sending a buncha data to discord...")
+                    # await channel.send(f"_{data.get('name', '[UNKNOWN]')}_", embed=embed)
+                    await channel.send('hi!')
+                    print('          finished send')
+
+                except KeyError as e:
+                    print(e)
+        else:
+            print(f"{room_name}: Not found in current list of rooms... what?")
+
+        print('-- finished handling a roll stream event ---')
+
+
+    # ==================================================================
+    # ======================= TEXT COMMANDS ============================
+    # ==================================================================
+
+    @commands.command(pass_context=True)
+    async def add_room(ctx, room_name: str):
+        print(f"ADDING ROOM {room_name}")
+
+        global rooms
+        l_room_name = room_name.strip().lower()
+
+        if l_room_name in rooms.keys():
+            rooms[l_room_name] = rooms[l_room_name].append(ctx.message.channel.id)
+        else:
+            rooms[l_room_name] = [ctx.message.channel.id]
+            self.create_stream(l_room_name)
+
+        # Save the re-load room
+        open('rooms.json', 'w').write(json.dumps(rooms, indent=2))
+        rooms = json.load(open('rooms.json', 'r'))
+
+
+
+
+def parse_roll(roll_msg_data):
+    print(roll_msg_data)
+
+    content_text = ""  # goes in message content
+    decorative_text = ""  # goes in embed author name
+    result_text = ""  # goes in embed title
+    rolls_text = ""  # goes in embed description
+
+    summary_mode = roll_msg_data.get("conditions")  # total | high | low
+
+    # extracts the roll objects from the firebase data
+    all_rolls = [roll[1] for roll in roll_msg_data.items() if "roll" in roll[0]]
+
+    # Collect all results by dietype e.g. {'d4':[2,3,3], 'd20':[20]}
+    results_by_dietype = {}
+    for roll in all_rolls:
+        dietype = roll["dieType"]
+        result = roll["result"]
+        if dietype in results_by_dietype:
+            results_by_dietype[dietype].append(result)
+        else:
+            results_by_dietype[dietype] = [result]
+
+    # ~ character name ~
+    content_text = roll_msg_data.get("name", "[UNKNOWN]")
+
+    # ~ extra decorative text ~
+    decorative_text = "🌺 💀 🌺"
+
+    # ~ summary // total ~
+    if summary_mode == "total":
+        all_values = [val["result"] for val in all_rolls if "result" in val.keys()]
+        result_text = str(sum(all_values)).rjust(2, " ")
+
+    if summary_mode == "high":
+        high_values = [max(results) for results in results_by_dietype.values()]
+        result_text = str(sum(high_values)).rjust(2, " ")
+
+    if summary_mode == "low":
+        low_values = [min(results) for results in results_by_dietype.values()]
+        result_text = str(sum(low_values)).rjust(2, " ")
+
+    result_text = f"-{{ {result_text} }}-"
+
+    # ~ what-was-rolled graph ~    (or one-liner if it was a simple roll)
+    if len(all_rolls) == 1:
+        rolls_text = f"```-{{ {list(results_by_dietype)[0]} }}-```"
+
+    else:
+        # first "column" is five spaces wide, "total" | "  min" | "  max"
+        type_line = " " * 5
+        mode_text = {"low": "min", "high": "max", "total": "total"}
+        results_line = (
+            mode_text[summary_mode] if summary_mode in mode_text else ""
+        ).rjust(5, " ")
+
+        # the following columns are: die type on top, results grouped below.
+        group_join_char = "+" if summary_mode == "total" else ","
+        for dietype in results_by_dietype:
+
+            # die type is easy
+            type_column = str(dietype)
+
+            # results need to be joined together with either a comma or plus, depending on the summation mode
+            result_strings = [str(result) for result in results_by_dietype[dietype]]
+            result_column = f"({group_join_char.join(result_strings)})"
+
+            # everything but the last group gets a +
+            if dietype != list(results_by_dietype)[-1]:
+                result_column += " +"
+
+            # keep the column the same length by padding the shorter row with spaces
+            column_width = max(len(type_column), len(result_column))
+            type_column = type_column.ljust(column_width, " ")
+            result_column = result_column.ljust(column_width, " ")
+
+            type_line += f" {type_column}"
+            results_line += f" {result_column}"
+
+        rolls_text = f"```{type_line}\n{results_line}```"
+
+    # print(content_text)
+    # print(decorative_text)
+    # print(result_text)
+    # print(rolls_text)
+
+    # TODO: content_text needs to get to the bot.say() somehow; either return it alongside the embed or just do the .get("name") out there.
+    embed = discord.Embed(
+        title=result_text,
+        description=rolls_text,
+        color=discord.Color(int("0x511D20", 16)),
+    )  # Mutter the incantation and perform the hex
+    embed.set_author(name=decorative_text)
+
+    # embed.set_footer(text="room-name-here")
+    # embed.set_image()
+    # embed.set_thumbnail()
+
+    # Can't have empty fiends or it gets mad. { Olive: that github visualizer seemed to think that [] was an ok value for "fields"? Can we skip it? }
+    # if field_text == "":
+    #     field_text = "[UNK]"
+    # if embed_text.strip() == "":
+    #     embed_text = "[UNK]"
+    #
+    # embed.add_field(name=field_text, value=embed_text, inline=False)
+
+    return embed
