15512aa350f970d61a66f3c232d307b35441a5ac
Olive Perry <wick@nitidbit.com>
2022-02-02 00:59:13 -0800

    WIP adding firebase

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
 const { Client, Intents } = require('discord.js');
 const fs = require('fs');
+const util = require('util');
 require('dotenv').config();
 
+const { initializeApp } = require("firebase/app");
+const { getDatabase } = require("firebase/database");
 
-// GLOBAL VARS
-const WITCHBOT_TOKEN = process.env.WITCHBOT_TOKEN
+var firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "roll-to-hit.firebaseapp.com",
+  databaseURL: "https://roll-to-hit.firebaseio.com",
+  projectId: "roll-to-hit",
+  storageBucket: "roll-to-hit.appspot.com",
+  messagingSenderId: "55772958032",
+  appId: "1:55772958032:web:42c2dc78d955a887293055",
+};
+const firebaseApp = initializeApp(firebaseConfig);
+const firebaseDB = getDatabase(firebaseApp)
 
+// Development: point to the local firebase emulator instead of production
+// if (process.env.WITCHBOT_TOKEN === "development") {
+//   firebase.database().useEmulator("localhost", 9000);
+// }
+
+// Convert fs.readFile into Promise version of same
+const readFile = (fileName) => util.promisify(fs.readFile)(fileName, 'utf8');
+
+
+
+// ------------------------------------
+// ------------ GLOBAL VARS ------------
+// ------------------------------------
 const CONNECTED_CHANNEL_FILE = 'rooms.json'
-let allConnectedChannels = {}
-
-// ------------------------------------
-// ------------ CREATE THE BOT ------------
-// ------------------------------------
-
 const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+let allConnectedChannels = {}
 
 // ------------------------------------
 // ------------ EVENTS ------------
@@ -22,15 +42,29 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 // When the client is ready, run this code (only once)
 client.once('ready', () => {
   console.info(`Logged in as ${client.user}!`);
+  // initializeBot()
 
-  // populate connected rooms from the local json file
-  fs.readFile(CONNECTED_CHANNEL_FILE, (err, data) => {
-      if (err) throw err;
-      allConnectedChannels = JSON.parse(data);
-      console.log('Initialized room connections');
-      console.log(allConnectedChannels);
-  });
+  loadAllConnectedChannels()
 });
+
+// populate connected rooms from the local json file
+async function loadAllConnectedChannels() {
+  console.log('Loading connected channels....');
+
+  const channelData = await readFile(CONNECTED_CHANNEL_FILE)
+  allConnectedChannels = JSON.parse(channelData);
+
+  console.log('Initialized room connections');
+  console.log(allConnectedChannels);
+
+  console.log('Listening for rolls....');
+  Object.keys(allConnectedChannels).forEach(channelID => {
+    listenForRolls(channelID, allConnectedChannels[channelID])
+  });
+}
+
+
+
 
 client.on('interactionCreate', async interaction => {
   // console.log('triggered interaction!', interaction);
@@ -49,7 +83,7 @@ client.on('interactionCreate', async interaction => {
 
 
 // Login to Discord with your client's token
-client.login(WITCHBOT_TOKEN);
+client.login(process.env.WITCHBOT_TOKEN);
 
 
 // ------------------------------------
@@ -66,12 +100,16 @@ function joinRoom(channelID, roomName){
       replyString = `This channel is now in the room '${roomName}'.`
       allConnectedChannels[channelID] = roomName
       saveConnectedChannels()
+      listenForRolls(channelID, roomName)
 
     // It switched from another room to this one
     } else if (allConnectedChannels[channelID] !== roomName) {
       replyString = `This channel has switched from the room '${allConnectedChannels[channelID]}' to '${roomName}'.`
       allConnectedChannels[channelID] = roomName
       saveConnectedChannels()
+      listenForRolls(channelID, roomName)
+
+      // TODO: leave the previous channel
 
     } else {
       replyString = `This channel was already in the room '${allConnectedChannels[channelID]}'!`
@@ -81,10 +119,24 @@ function joinRoom(channelID, roomName){
   return replyString
 }
 
+// set up the listeners for a room
+function listenForRolls(channelID, roomName) {
+  console.log('    Creating listeners for room', roomName,' to channel ', channelID);
+  // const channel = client.channels.cache.get(channelID);
+
+  const dbRollsRef = firebaseDB.ref().child('rolls').child(roomName)
+  dbRollsRef.on('child_changed', (snapshot) => {
+    if (snapshot) { console.log('roll changed!', snapshot.val()) }
+  });
+  dbRollsRef.on('child_added', (snapshot) => {
+    if (snapshot) { console.log('roll added!', snapshot.val()) }
+  });
+}
 
 // ------------------------------------
 // ------------ UTILS ------------
 // ------------------------------------
+
 
 
 // POTENTIAL RACE CONDITION HERE!!!!
